154ca928b5bd02bc464abb83e1accf4bae24ef5f
Sebastian Salazar <sasalazar10@espe.edu.ec>
2024-06-08 19:58:13 -0500

    v1

--- bank/src/main/java/com/banquito/core/bank/repository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.banquito.core.bank.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.banquito.core.bank.model.Bank;
-
-public interface BankRepository extends JpaRepository<Bank, String>{
-
-}
